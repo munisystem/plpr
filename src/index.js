9e8fc7baa3096baa42f5592fd6338a75758f326d
Yuichi Saito <munisystem@gmail.com>
2017-04-07 16:59:40 +0900

    Support log_line_prefix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 'use strict';
 
-const REGEX = /(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.?\d* \D{3}).* duration: (\d+\.\d+) ms  execute .*: (.+)?/;
-var objs = [];
+const regexBase = 'LOG:  duration: (\\d+\\.\\d+) ms  execute .*: (.+)?';
+const regexps = {
+  '%u': ['user', '([0-9a-zA-Z\\.\\-\\_\\[\\]]*)?'],
+  '%d': ['dbname','([0-9a-zA-Z\\.\\-\\_\\[\\]]*)?'],
+  '%r': ['hostport', '(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\(\\d+\\))?'],
+  '%h': ['host', '(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})?'],
+  '%p': ['pid', '(\\d+)*'],
+  '%t': ['timestamp', '(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} \\D{3})'],
+  '%m': ['mtimestamp', '(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\\.\\d* \\D{3})'],
+};
 
-module.exports = data => {
+var objs = [];
+var obj_keys = [];
+var regex = null;
+
+module.exports = (data, format) => {
+  format = format.replace(/\./, '\\.')
+                 .replace(/\-/, '\\-')
+                 .replace(/\_/, '\\_')
+                 .replace(/\*/, '\\*')
+                 .replace(/\?/, '\\?')
+                 .replace(/\^/, '\\^')
+                 .replace(/\$/, '\\$')
+                 .replace(/\+/, '\\+')
+                 .replace(/\[/, '\\[')
+                 .replace(/\]/, '\\]')
+                 .replace(/\{/, '\\{')
+                 .replace(/\}/, '\\}');
+
+  while (format.match(/%[udrhptm]/) !== null) {
+    var match = format.match(/%[udrhptm]/);
+    format = format.replace(match, regexps[match][1]);
+    obj_keys.push(regexps[match][0]);
+  }
+
+  regex = new RegExp(format+regexBase);
+
   const lines = data.split(/\r\n|\r|\n/);
 
   var content = [];
@@ -24,8 +57,14 @@ module.exports = data => {
 }
 
 function parser(data) {
-  const matches = data.match(REGEX);
+  const matches = data.match(regex);
   if (matches !== null) {
-    objs.push({timestamp: Date.parse(matches[1]), duration: Number(matches[2]), query: matches[3]});
+    var obj = {};
+    for (var i = 0; i < obj_keys.length; i++) {
+      obj[obj_keys[i]] = matches[i+1];
+    }
+    obj['duration'] = matches[obj_keys.length+1];
+    obj['query'] = matches[obj_keys.length+2];
+    objs.push(obj);
   }
 }
